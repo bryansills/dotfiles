c4392699901ac00e4b8b7c9bffe3aabab2723b79
bryansills <bsills@bignerdranch.com>
2022-08-24 17:48:34 -0500

    Update Homebrew setup

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,7 +12,7 @@ then
 
   if test "$(uname)" = "Darwin"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
 fi
